cb7616623f58cd40f9c1b4ae509dbb0af47c315a
Hanglesson Fontines <72349939+Hanglesson@users.noreply.github.com>
2020-10-06 01:34:01 -0500

    :package: Header

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // // scroll-up button show/hide script
+        // if(this.scrollY > 500){
+        //     $('.scroll-up-btn').addClass("show");
+        // }else{
+        //     $('.scroll-up-btn').removeClass("show");
+        // }
+    });
+    
+    //toggle menu/navbar sript
+    $(".btn-menu").click(function(){
+        $(".navbar .menu").toggleClass("active");
+        $(".btn-menu i").toggleClass("active");
+    });
+
+});
