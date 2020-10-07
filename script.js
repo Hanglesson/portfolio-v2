84bba269d7836cebcd9a67a25e078a9e286212d2
Hanglesson Fontines <72349939+Hanglesson@users.noreply.github.com>
2020-10-06 22:18:40 -0500

    :package: Sessão Minha Equipe

--- script.js
+++ script.js
@@ -21,4 +21,26 @@ $(document).ready(function(){
         $(".btn-menu i").toggleClass("active");
     });
 
+    // owl carrosel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
 });
