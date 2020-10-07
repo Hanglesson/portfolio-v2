b2231860147585254856b6e50bea209881165c79
Hanglesson Fontines <72349939+Hanglesson@users.noreply.github.com>
2020-10-07 01:06:42 -0500

    :package: Scroll Up e Animação

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
-        // // scroll-up button show/hide script
-        // if(this.scrollY > 500){
-        //     $('.scroll-up-btn').addClass("show");
-        // }else{
-        //     $('.scroll-up-btn').removeClass("show");
-        // }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
-    
+
+    //Slide-up Script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({
+            scrollTop: 0
+        });
+    });
+
     //toggle menu/navbar sript
-    $(".btn-menu").click(function(){
+    $(".btn-menu").click(function () {
         $(".navbar .menu").toggleClass("active");
         $(".btn-menu i").toggleClass("active");
     });
+
+    //typing animation
+    var typed = new Typed(".typing", {
+        strings: ["Desenvolvedor", "Front End", "Designer Gráfico", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Desenvolvedor", "Front End", "Designer Gráfico", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
 
     // owl carrosel script
     $('.carousel').owlCarousel({
@@ -28,15 +51,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
